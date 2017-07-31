626298392b763d2b687e766020b967113f2de57c
Aakil Fernandes <aakilfernandes@gmail.com>
2017-07-31 16:25:36 -0400

    use jobbit for run-jobs

--- scripts/run-jobs.js
+++ scripts/run-jobs.js
@@ -1,13 +1,58 @@
+const Jobbit = require('jobbit')
+const _ = require('lodash')
+const connection = require('../lib/connection')
+const delay = require('delay')
+
 console.log('run-jobs')
 
-const JobQueue = require('../lib/JobQueue')
-const get_reddit_posts_job = require('../jobs/getRedditPosts')
+const script_dir = `node ${__dirname}/../scripts`
 
-const job_queue = new JobQueue()
+let add_reddit_posts_started_at = null
 
-job_queue.addJob(get_reddit_posts_job)
-setInterval(() => {
-  job_queue.addJob(get_reddit_posts_job)
-}, 10000)
+_.range(4).map(() => {
+  return runJobbitThread()
+})
 
-job_queue.runJobs()
+function getCommand(script) {
+  return `node ${__dirname}/../scripts/${script}.js`
+}
+
+function runJobbitThread() {
+  return getNextJobbit().finally(runJobbitThread)
+}
+
+function getNextJobbit() {
+
+  const script_name = getNextScriptName()
+  return connection.query('INSERT INTO jobs(created_at, name) VALUES(?, ?)', [
+    new Date(), script_name
+  ]).then((results) => {
+    const job_id = results.insertId
+    const command = getCommand(script_name)
+    const jobbit = new Jobbit(command)
+
+    return jobbit.promise.then((completion) => {
+      return connection.query('UPDATE jobs SET finished_at = ?, is_failed = ?, error = ? WHERE id = ?', [
+        new Date(),
+        (completion.error || completion.stderr) ? true : false,
+        completion.stderr || completion.error || null,
+        job_id
+      ])
+    }, (error) => {
+      return connection.query('UPDATE jobs SET finished_at = ?, is_failed = ?, error = ? WHERE id = ?', [
+        new Date(),
+        true,
+        error,
+        job_id
+      ])
+    })
+  })
+}
+
+function getNextScriptName() {
+  if (add_reddit_posts_started_at === null || Date.now() - add_reddit_posts_started_at > 10000) {
+    add_reddit_posts_started_at = Date.now()
+    return 'add-reddit-posts'
+  }
+  return 'scrape-url'
+}
