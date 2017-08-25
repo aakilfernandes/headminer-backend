1165efcea88ac9d4e2375c577dffc814c4d07b4d
Aakil Fernandes <aakilfernandes@gmail.com>
2017-08-25 19:09:33 -0400

    rm test

--- test.js
+++
@@ -1,8 +0,0 @@
-const connection = require('./lib/connection')
-
-connection.query('SELECT * FROM urls WHERE id = ?', [253]).then((urls2) => {
-  return connection.query('SELECT * FROM urls WHERE id = ?', [253]).then((urls) => {
-    const url = urls[0]
-    console.log(url)
-  })
-})
