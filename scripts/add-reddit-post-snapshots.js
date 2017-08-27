266e07859b3bdaa7f144d8d0dc6b0536c589d29c
Aakil Fernandes <aakilfernandes@gmail.com>
2017-08-27 19:45:09 -0400

    set snapshot_added_at quicker and only 10 snapshots

--- scripts/add-reddit-post-snapshots.js
+++ scripts/add-reddit-post-snapshots.js
@@ -4,53 +4,64 @@ const getRedditPosts = require('../lib/getRedditPosts')
 const getQGroups = require('../lib/getQGroups')
 const urljs = require('url')
 const waterfall = require('promise-waterfall')
+const _ = require('lodash')
+const getQs = require('../lib/getQs')
 
 return connection.query(`
   SELECT reddit_posts.* FROM reddit_posts, urls, domains
     WHERE reddit_posts.url_id = urls.id
       AND urls.domain_id = domains.id
       AND domains.is_ignored = 0
-    ORDER BY snapshot_added_at ASC LIMIT 100;
+    ORDER BY snapshot_added_at ASC LIMIT 10;
 `).then((posts) => {
 
-  const bodies = []
+  const post_ids = _.map(posts, 'id')
+  const post_ids_qs = getQs(post_ids.length)
 
-  const fetches = posts.map((post) => {
-    return function fetch() {
-      const api_url = `https://www.reddit.com/r/all/comments/${post.id}.json`
-      return request(api_url).then((body) => {
-        bodies.push(body)
-      })
-    }
-  })
+  return connection.query(`
+    UPDATE reddit_posts SET snapshot_added_at = NOW() WHERE id IN (${post_ids_qs})
+  `, post_ids).then(() => {
 
-  return waterfall(fetches).then(() => {
+    const bodies = []
 
-    const queries = []
-    const values = []
-
-    bodies.forEach((body, index) => {
-      const data = JSON.parse(body)[0].data.children[0].data
-      const score = data.score
-      const comments_count = data.num_comments
-      const post = posts[index]
-
-      queries.push(`
-        UPDATE reddit_posts
-          SET score = ?, comments_count = ?, snapshot_added_at = NOW()
-          WHERE id = ?;
-        INSERT INTO reddit_post_snapshots(post_id, score, comments_count)
-          VALUES(?, ?, ?);
-      `)
-      values.push(
-        score, comments_count, post.id,
-        post.id, score, comments_count
-      )
+    const fetches = posts.map((post) => {
+      return function fetch() {
+        const api_url = `https://www.reddit.com/r/all/comments/${post.id}.json`
+        return request(api_url).then((body) => {
+          bodies.push(body)
+        })
+      }
     })
 
-    const query = queries.join('\r\n')
+    return waterfall(fetches).then(() => {
 
-    return connection.query(query, values)
+      const queries = []
+      const values = []
+
+      bodies.forEach((body, index) => {
+        const data = JSON.parse(body)[0].data.children[0].data
+        const score = data.score
+        const comments_count = data.num_comments
+        const post = posts[index]
+
+        queries.push(`
+          UPDATE reddit_posts
+            SET score = ?, comments_count = ?
+            WHERE id = ?;
+          INSERT INTO reddit_post_snapshots(post_id, score, comments_count)
+            VALUES(?, ?, ?);
+        `)
+        values.push(
+          score, comments_count, post.id,
+          post.id, score, comments_count
+        )
+      })
+
+      const query = queries.join('\r\n')
+
+      return connection.query(query, values)
+    })
+
   })
 
 }).finally(() => {
