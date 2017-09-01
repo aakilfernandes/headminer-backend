2d5cb93123a49f400187fbe7568bab48721c6873
Aakil Fernandes <aakilfernandes@gmail.com>
2017-08-31 22:46:05 -0400

    update ajdusted_influence on twitter-influencify-articles

--- scripts/twitter-influencify-articles.js
+++ scripts/twitter-influencify-articles.js
@@ -14,7 +14,7 @@ connection.query(`
     UPDATE articles SET influencified_at = NOW() WHERE id IN (${article_ids_qs});
     SELECT twitter_urls_influences.*, article_id FROM twitter_urls_influences, urls
     WHERE twitter_urls_influences.url_id = urls.id
-      AND urls.article_id IN (${article_ids_qs})
+      AND urls.article_id IN (${article_ids_qs});
   `, article_ids.concat(article_ids)).then((results) => {
     const urls_influences = results[1]
     if (urls_influences.length === 0) {
@@ -39,6 +39,20 @@ connection.query(`
         values.push(article.id, influencer_id, influence, influence)
       })
     })
+
+    const article_ids = _.map(articles, 'id')
+    const article_ids_qs = getQs(article_ids.length)
+    queries.push(`
+      UPDATE twitter_articles_influences
+      SET adjusted_influence = (
+        SELECT (twitter_articles_influences.influence - (twitter_influencers.average_influence * articles.twitter_statuses_count)) / twitter_influencers.followers_count
+        FROM twitter_influencers, articles
+        WHERE twitter_articles_influences.article_id = articles.id
+        	AND twitter_articles_influences.influencer_id = twitter_influencers.id
+      )
+      WHERE twitter_articles_influences.article_id IN (${article_ids_qs});
+    `)
+    values.push(...article_ids)
 
     const query = queries.join('\r\n')
     return connection.query(query, values)
--- scripts/update-average-penetration.js
+++ scripts/update-average-penetration.js
@@ -7,26 +7,25 @@ connection.query(`
   SELECT * FROM twitter_influencers WHERE is_ignored = 0;
 `).then((influencers) => {
 
+  const influencers_by_id = _.keyBy(influencers, 'id')
   const queries = []
   const values = []
 
   const getQueriesAndValues = influencers.map((influencer) => {
     return function getQueriesAndValues() {
       return connection.query(`
-        SELECT articles.*, twitter_articles_influences.influence  FROM twitter_articles_influences, articles
+        SELECT articles.twitter_statuses_count, twitter_articles_influences.influence  FROM twitter_articles_influences, articles
         WHERE twitter_articles_influences.influencer_id = ?
           AND twitter_articles_influences.article_id = articles.id
         ORDER BY rand()
         LIMIT 1000
       `, [influencer.id]).then((results) => {
-        let twitter_statuses_count = 0
-        let influence = 0
+        let penetration = 0
         results.forEach((result) => {
-          twitter_statuses_count += result.twitter_statuses_count
-          influence += result.influence
+          penetration += result.penetration
         })
 
-        queries.push(`UPDATE twitter_influencers SET average_influence = ? WHERE id = ?;`);
+        queries.push(`UPDATE twitter_influencers SET average_penetration = ? WHERE id = ?;`);
         values.push(influence / twitter_statuses_count, influencer.id)
       })
     }
