0c87f17d26f4eb11c20519dca9b073eee6cd5d16
Aakil Fernandes <aakilfernandes@gmail.com>
2017-09-03 14:13:36 -0400

    waterfall proxies

--- scripts/add-facebook-snapshots.js
+++ scripts/add-facebook-snapshots.js
@@ -10,7 +10,43 @@ const updateFacebookLimitedAt = require('../lib/updateFacebookLimitedAt')
 const fs = require('fs')
 const getSecret = require('../lib/getSecret')
 
-const proxies = getSecret('proxies').split('\n')
+const proxies = _.shuffle(getSecret('proxies').split('\n'))
+let proxy_index = 0
+
+function fetchAndPush(url_pojo, queries, values) {
+  const proxy = proxies[proxy_index]
+  const proxyRequest = request.defaults({ proxy })
+  return proxyRequest(`http://graph.facebook.com/?id=${url_pojo.url}`).then((body) => {
+    queries.push(`
+      UPDATE urls SET og_id = ?, facebook_share_count = ?, facebook_comment_count = ? WHERE id = ?;
+      INSERT INTO facebook_snapshots(url_id, updated_time, share_count, comment_count)
+        VALUES(?, ?, ?, ?);
+    `)
+    const og_pojo = JSON.parse(body)
+    const og_id = og_pojo.og_object ? og_pojo.og_object.id : null
+    const updated_time = og_pojo.og_object ? new Date(og_pojo.og_object.updated_time) : null
+    values.push(
+      og_id,
+      og_pojo.share.share_count,
+      og_pojo.share.comment_count,
+      url_pojo.id,
+      url_pojo.id,
+      updated_time,
+      og_pojo.share.share_count,
+      og_pojo.share.comment_count
+    )
+  }, (error) => {
+    if (error && error.statusCode === 403) {
+      proxy_index += 1
+      if (proxy_index >= proxies.length) {
+        updateFacebookLimitedAt()
+        throw error
+      }
+      return fetchAndPush(url_pojo, queries, values)
+    }
+    throw error
+  })
+}
 
 return connection.query(`
   SELECT urls.* FROM urls, domains
@@ -26,47 +62,23 @@ return connection.query(`
     const queries = []
     const values = []
 
-    const fetches = url_pojos.map((url_pojo) => {
-      queries.push(`
-        UPDATE urls SET og_id = ?, facebook_share_count = ?, facebook_comment_count = ? WHERE id = ?;
-        INSERT INTO facebook_snapshots(url_id, updated_time, share_count, comment_count)
-          VALUES(?, ?, ?, ?);
-      `)
-      return function fetch() {
-        const proxy = _.sample(proxies)
-        console.log(proxy)
-        const proxyRequest = request.defaults({ proxy })
-        return proxyRequest(
-          `http://graph.facebook.com/?id=${url_pojo.url}`
-        ).then((body) => {
-          const og_pojo = JSON.parse(body)
-          const og_id = og_pojo.og_object ? og_pojo.og_object.id : null
-          const updated_time = og_pojo.og_object ? new Date(og_pojo.og_object.updated_time) : null
-          values.push(
-            og_id,
-            og_pojo.share.share_count,
-            og_pojo.share.comment_count,
-            url_pojo.id,
-            url_pojo.id,
-            updated_time,
-            og_pojo.share.share_count,
-            og_pojo.share.comment_count
-          )
-        })
+    const wrappedFetchAndPushes = url_pojos.map((url_pojo) => {
+      return () => {
+        return fetchAndPush(url_pojo, queries, values)
       }
     })
 
-    return waterfall(fetches).then(() => {
+    return waterfall(wrappedFetchAndPushes).then(() => {
       const query = queries.join('\r\n')
       return connection.query(query, values)
     }, (error) => {
       if (error && error.statusCode === 403) {
-        updateFacebookLimitedAt()
+        const query = queries.join('\r\n')
+        return connection.query(query, values).then(() => {
+          throw error
+        })
       }
-      const query = queries.join('\r\n')
-      return connection.query(query, values).then(() => {
-        throw error
-      })
+      throw error
     })
   })
 }).finally(() => {
