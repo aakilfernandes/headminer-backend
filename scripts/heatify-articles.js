cd2238a6fff47028c67bd02398f906ba31c1d367
Aakil Fernandes <aakilfernandes@gmail.com>
2017-09-05 15:40:20 -0400

    use dynamic averages for heat

--- scripts/heatify-articles.js
+++ scripts/heatify-articles.js
@@ -5,35 +5,35 @@ return connection.query(`
   SELECT * FROM articles
   WHERE coallesced_at IS NOT NULL
   ORDER BY heatified_at ASC, id ASC LIMIT 1000;
-`).then((articles) => {
+
+  SELECT AVG(reddit_score) FROM articles;
+  SELECT AVG(twitter_statuses_count) FROM articles;
+  SELECT AVG(facebook_share_count) FROM articles;
+`).then((results) => {
+  const articles = results[0]
+  const average_reddit_score = results[1][0]['AVG(reddit_score)']
+  const average_twitter_statuses_count = results[2][0]['AVG(twitter_statuses_count)']
+  const average_facebook_share_count = results[3][0]['AVG(facebook_share_count)']
 
   const queries = []
   const values = []
 
   articles.forEach((article) => {
-    const average_reddit_post_count = 1.7
-    const average_twitter_statuses_count = 258.5
-    const average_facebook_share_count = 190
-    const average_facebook_comment_count = 0.12
 
-    const reddit_post_count_heat = article.reddit_posts_count / average_reddit_post_count
-    const reddit_score_count_heat = article.reddit_score
+    const reddit_score_count_heat = article.reddit_score / average_reddit_score
     const twitter_heat = article.twitter_statuses_count / average_twitter_statuses_count
     const facebook_share_count_heat = article.facebook_share_count / average_facebook_share_count
-    const facebook_comment_count_heat = article.facebook_comment_count_heat / average_facebook_comment_count
 
     const heat = [
       reddit_score_count_heat,
-      reddit_score_count_heat,
       twitter_heat,
-      twitter_heat,
-      facebook_share_count_heat,
-      facebook_comment_count_heat,
+      facebook_share_count_heat
     ].reduce((sum, _heat) => {
       if (_heat <= 0) {
         return sum
       }
-      return sum + Math.log10(1 + _heat)
+      const logged_heat = Math.log10(1 + _heat)
+      return sum + logged_heat
     }, 0).toFixed(4)
 
     queries.push('UPDATE articles SET heat = ?, heatified_at = NOW() WHERE id = ?;')
