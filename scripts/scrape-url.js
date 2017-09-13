257ec2e7c3929c44cb00118f4491cc9570fe04fb
Aakil Fernandes <aakilfernandes@gmail.com>
2017-09-13 13:52:47 -0400

    faster scrape url

--- scripts/scrape-url.js
+++ scripts/scrape-url.js
@@ -6,112 +6,126 @@ const _ = require('lodash')
 const getQGroups = require('../lib/getQGroups')
 const urljs = require('url')
 const JSDOM = require('jsdom').JSDOM
+const Promise = require('bluebird')
 
 let hostname_pojos
 
 return mysqlQuery(`
-  START TRANSACTION;
-  SET @url_id := (
-    SELECT urls.id FROM domains, urls
-      WHERE
-        domains.id = urls.domain_id
-        AND domains.is_ignored = FALSE
-        AND urls.article_id IS NULL
-        AND urls.created_at > NOW() - INTERVAL 48 HOUR
-      ORDER BY scraped_at ASC
-      LIMIT 1
-  );
-  SELECT * FROM urls WHERE id = @url_id;
-  UPDATE urls SET scraped_at = NOW() WHERE id = @url_id;
-  COMMIT;
-  `
-).then((results) => {
-  const url_pojo = results[2][0]
-  if (!url_pojo) {
+  SELECT * FROM domains, urls
+  WHERE domains.id = urls.domain_id
+    AND domains.is_ignored = 0
+    AND urls.article_id IS NULL
+    AND urls.created_at > NOW() - INTERVAL 48 HOUR
+  ORDER BY scraped_at ASC
+  LIMIT 1
+`).then((url_pojos) => {
+  if (url_pojos.length === 0) {
     return
   }
+  const url_pojo = url_pojos[0]
   console.log(url_pojo.id)
-  return request(url_pojo.url, {
-    resolveWithFullResponse: true,
-    timeout: 5000
-  }).then((response) => {
+  return mysqlQuery(`
+    UPDATE urls SET scraped_at = NOW() where id = ?
+  `, [url_pojo.id]).then(() => {
+    return request(url_pojo.url, {
+      resolveWithFullResponse: true,
+      timeout: 5000
+    }).then((response) => {
 
-    const parsed = parseHtml(response.body)
-    const urls = _.uniq([parsed.meta['og:url'], parsed.links.canonical, response.request.uri, url_pojo.url].filter((url) => {
-      return typeof url === 'string'
-    })).map((url) => {
-      const protocol = url_pojo.url.split('://')[0]
-      if (url.indexOf('//') === 0) {
-        return url.replace('//', `${protocol}://`)
+      const parsed = parseHtml(response.body)
+      const urls = _.uniq([parsed.meta['og:url'], parsed.links.canonical, response.request.uri, url_pojo.url].filter((url) => {
+        return typeof url === 'string'
+      })).map((url) => {
+        const protocol = url_pojo.url.split('://')[0]
+        if (url.indexOf('//') === 0) {
+          return url.replace('//', `${protocol}://`)
+        }
+        return url
+      })
+
+      let special_title = null
+
+      if (url_pojo.domain_id === 429) {
+        const jsdom = new JSDOM(response.body)
+        special_title = jsdom.window.document.querySelector('.headline').textContent
       }
-      return url
-    })
 
-    let special_title = null
+      const title = special_title || parsed.meta['og:title'] || parsed.title
+      const author = parsed.meta['article:author'] || parsed.meta.author
+      const description = parsed.meta['og:description'] || parsed.description
+      const image = parsed.meta['og:image']
+      const canonical_url = urls[0]
+      const canonical_url_domain = urljs.parse(canonical_url).hostname
+      const is_canonical = (url_pojo.url === canonical_url)
+      console.log(is_canonical)
 
-    if (url_pojo.domain_id === 429) {
-      const jsdom = new JSDOM(response.body)
-      special_title = jsdom.window.document.querySelector('.headline').textContent
-    }
+      let article_insert
 
-    const title = special_title || parsed.meta['og:title'] || parsed.title
-    const author = parsed.meta['article:author'] || parsed.meta.author
-    const description = parsed.meta['og:description'] || parsed.description
-    const image = parsed.meta['og:image']
-    const canonical_url = urls[0]
-    const canonical_url_domain = urljs.parse(canonical_url).hostname
+      if (url_pojo.article_id === null) {
+        article_insert = mysqlQuery(`
+          INSERT INTO articles(title, author, description, image) VALUES (?, ?, ?, ?);
+        `, [
+          title,
+          author,
+          description,
+          image
+        ])
+      } else {
+        article_insert = mysqlQuery(`
+          UPDATE articles SET title = ?, author = ?, description = ?, image = ? where id = ?
+        `, [
+          title,
+          author,
+          description,
+          image,
+          url_pojo.article_id
+        ])
+      }
 
-    let article_promise
+      return article_insert.then((results) => {
 
-    if (url_pojo.article_id === null) {
-      article_promise = mysqlQuery(`
-        INSERT INTO articles(title, author, description, image) VALUES (?, ?, ?, ?);
-      `, [
-        title,
-        author,
-        description,
-        image
-      ])
-    } else {
-      article_promise = mysqlQuery(`
-        UPDATE articles SET title = ?, author = ?, description = ?, image = ? where id = ?
-      `, [
-        title,
-        author,
-        description,
-        image,
-        url_pojo.article_id
-      ])
-    }
+        const article_id = url_pojo.article_id ? url_pojo.article_id : results.insertId
 
-    return article_promise.then((results) => {
-      const article_id = url_pojo.article_id ? url_pojo.article_id : results.insertId
-      console.log(article_id)
-      return mysqlQuery(`
-        START TRANSACTION;
-        INSERT IGNORE INTO domains(domain) VALUES (?);
-        INSERT IGNORE INTO urls(domain_id, url, article_id, scraped_at) VALUES (
-          (SELECT id FROM domains WHERE domains.domain = ?),
-          ?,
-          ?,
-          NOW()
-        );
-        SET @canonical_url_id := (SELECT id FROM urls WHERE url = ? LIMIT 1);
-        UPDATE urls
-          SET canonical_url_id = @canonical_url_id,
-          article_id = ?
-          WHERE id = ?;
-        UPDATE urls
-          SET canonical_url_id = @canonical_url_id
-          WHERE id = @canonical_url_id;
-        COMMIT;
-      `, [
-        canonical_url_domain,
-        canonical_url_domain, canonical_url, article_id,
-        canonical_url,
-        article_id,
-        url_pojo.id
-      ])
+        console.log(article_id)
+
+        let canonical_url_insert = null
+
+        if (is_canonical) {
+          canonical_url_insert = Promise.resolve()
+        } else {
+          canonical_url_insert = mysqlQuery(`
+            INSERT IGNORE INTO domains(domain) VALUES (?);
+            INSERT IGNORE INTO urls(domain_id, url, article_id, scraped_at) VALUES (
+              (SELECT id FROM domains WHERE domains.domain = ? LIMIT 1),
+              ?,
+              ?,
+              NOW()
+            );
+          `, [
+            canonical_url_domain,
+            canonical_url_domain, canonical_url, article_id
+          ])
+        }
+
+        return canonical_url_insert.then((results) => {
+
+          let update_urls_query = `
+            UPDATE urls SET canonical_url_id = ?, article_id = ? WHERE id = ?;
+          `
+          const canonical_url_id = is_canonical ? url_pojo.id : results[1].insertId
+          console.log(canonical_url_id)
+          const update_urls_values = [canonical_url_id, article_id, url_pojo.id]
+
+          if (!is_canonical) {
+            update_urls_query += `
+              UPDATE urls SET canonical_url_id = ? WHERE id = ?;
+            `
+            update_urls_values.push(canonical_url_id, canonical_url_id)
+          }
+
+          return mysqlQuery(update_urls_query, update_urls_values)
+        })
+      })
     })
   })
 }).finally(() => {
