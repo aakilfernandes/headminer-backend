2872ac036bcf40bd6cc2518350757eef8e9edf08
Aakil Fernandes <aakilfernandes@gmail.com>
2017-09-14 21:19:43 -0400

    add article snapshot after heatify

--- scripts/heatify-articles.js
+++ scripts/heatify-articles.js
@@ -63,7 +63,16 @@ return mysqlQuery(`
 
     const query = queries.join('\r\n')
 
-    return mysqlQuery(query, values)
+    return mysqlQuery(query, values).then(() => {
+      return mysqlQuery(`
+        INSERT INTO article_snapshots(article_id, heat, reddit_posts_count, reddit_score, twitter_statuses_count, facebook_share_count, facebook_comment_count)
+        SELECT id, heat, reddit_posts_count, reddit_score, twitter_statuses_count, facebook_share_count, facebook_comment_count
+          FROM articles
+          WHERE created_at > NOW() - INTERVAL 48 HOUR
+          	AND coallesced_at IS NOT NULL
+            AND id IN (${article_ids_qs})
+      `, article_ids)
+    })
   })
 }).finally(() => {
   return mysqlDisconnect()
