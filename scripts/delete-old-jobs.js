cbaca0f37efea6bbca9d5f8beab5a55fce5af411
Aakil Fernandes <aakilfernandes@gmail.com>
2017-09-20 14:40:19 -0400

    delete old jobs every hour

---
+++ scripts/delete-old-jobs.js
@@ -0,0 +1,4 @@
+const mysqlQuery = require('../lib/mysqlQuery')
+const mysqlDisconnect = require('../lib/mysqlDisconnect')
+
+mysqlQuery(`DELETE FROM jobs WHERE created_at < NOW() - INTERVAL 1 DAY`).finally(mysqlDisconnect)
